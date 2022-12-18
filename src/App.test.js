4773251df49f4e302b928e823ca0d6158be1fd04
Pathara Norasethasopon <patharanor@gmail.com>
2022-12-18 14:08:18 +0700

    Fixed ReferenceError: React is not defined
    Got error message
    
    ```sh
     FAIL  src/screens/Quiz.test.js (7.386s)
      ● renders unique user name
    
        ReferenceError: React is not defined
    ...
    ```
    
    Fixed by adding :
    
    ```js
    import React from 'react';
    ```
    
    on the top of the test script.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App.jsx';
 
-test('renders learn react link', () => {
+test('renders start button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole('button', { name: /start/i });
   expect(linkElement).toBeInTheDocument();
 });
